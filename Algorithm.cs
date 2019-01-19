83d7adb87ebfe6213dd9584662a570131ddbcd66
ThePinbot <mr-pinbot@gmx.at>
2019-01-19 17:52:00 +0100

    BasicGameofLive

---
+++ Algorithm.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace V1
+{
+    partial class LifeForm
+    {
+        // aOld Matrix mit der n-ten Generation
+        // aNew Matrix mit der n+1-ten Generation
+        void CalcNextGeneration(bool[,] aOld, bool[,] aNew)
+        {
+            _CC = aOld; // sicherstellen, daﬂ _CC=aOld ist
+
+            for (int i = 0; i < MAX_CELLS; i++)
+            {
+                for (int j = 0; j < MAX_CELLS; j++)
+                {
+                    //Eine tote Zelle mit genau drei lebenden Nachbarn wird in der Folgegeneration neu geboren.
+                    if (GetNeighbourCount(i, j) == 3 && _CC[i, j] == false)
+                        aNew[i, j] = true;
+
+                    else if (_CC[i, j] == true)
+                    {
+                        //Eine lebende Zelle mit zwei oder drei lebenden Nachbarn bleibt in der Folgegeneration am Lebe
+                        if (GetNeighbourCount(i, j) == 2 || GetNeighbourCount(i, j) == 3)
+                            aNew[i, j] = true;
+                        //Lebende Zellen mit mehr als drei lebenden Nachbarn sterben in der Folgegeneration an ‹berbevˆlkerung.
+                        else if (GetNeighbourCount(i, j) > 3)
+                            aNew[i, j] = false;
+                        //Lebende Zellen mit weniger als zwei lebenden Nachbarn sterben in der Folgegeneration an Einsamkeit.
+                        else if (GetNeighbourCount(i, j) < 2)
+                            aNew[i, j] = false;
+                    }
+                }
+
+            }
+
+            
+
+        }
+        void ClearCells(bool[,] aCells)//fertig
+        {
+            for (int i = 0; i < MAX_CELLS; i++)
+                for (int j = 0; j < MAX_CELLS; j++)
+                    aCells[i, j] = false;
+        }
+        void TurnCellOnOff(int aX, int aY)//Fertig
+        {
+            // Cell an der Stelle aX,aY toggeln ( Umschalten )
+            if (aX < MAX_CELLS && aY < MAX_CELLS)
+                _CC[aX, aY] = !_CC[aX, aY];
+
+        }
+        // cells of _CC
+        int GetNeighbourCount(int i, int j)//eventuell fertig
+        {
+            int summe = 0;
+
+            if (ValOf(i - 1, j - 1))
+                summe++;
+
+            if (ValOf(i, j - 1))
+
+                summe++;
+            if (ValOf(i + 1, j - 1))
+                summe++;
+
+            if (ValOf(i - 1, j))
+                summe++;
+
+            if (ValOf(i + 1, j))
+                summe++;
+
+            if (ValOf(i - 1, j + 1))
+                summe++;
+
+            if (ValOf(i, j + 1))
+                summe++;
+
+            if (ValOf(i + 1, j + 1))
+                summe++;
+
+
+            // wieviele lebende Nachbarn hat Cell(i,j)
+            return summe;
+        }
+
+        // Ist Cell(i,j) von _CC on oder off ?
+        // mit richtiger Behandlung von i,j<0 und i,j>=MAX_CELLS
+        bool ValOf(int i, int j)
+        {
+            if (i < 0)
+                i = MAX_CELLS - 1;
+
+            if (i > MAX_CELLS - 1)
+                i = 0;
+
+            if (j < 0)
+                j = MAX_CELLS - 1;
+
+            if (j > MAX_CELLS - 1)
+                j = 0;
+            return _CC[i, j];
+        }
+
+    }
+}
---
+++ LifeForm.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace V1
+{
+  public partial class LifeForm : Form
+  {
+    #region CONSTANTS
+    const int MAX_CELLS = 30;
+    const int CELL_SIZE = 20;
+    const int C_M = 2; // CellMargin
+        List<Point>_safeList = new List<Point>();
+    #endregion
+    
+    // 2 Matritzen zur Verwaltung der n'ten und der n+1'ten Generation
+    bool[,] _CA = new bool[MAX_CELLS, MAX_CELLS];
+    bool[,] _CB = new bool[MAX_CELLS, MAX_CELLS];
+    bool[,] _CC; // current ( active ) CellArray n'te Generation
+        
+    public LifeForm()
+    {
+      InitializeComponent();
+      timer1.Interval = 100;
+      // zum Testen ein paar Zellen setzen
+       //_CA[3, 3] = true; _CA[3, 4] = true; _CA[3, 5] = true;
+       //_CA[4, 3] = true; _CA[4, 4] = true; _CA[4, 5] = true;
+      _CC = _CA;
+    }
+
+    private void OnPanelPaint(object sender, PaintEventArgs e)
+    {
+      DrawGrid(e.Graphics);
+      DrawCells(e.Graphics);
+    }
+
+    private void OnPanelMouseDown(object sender, MouseEventArgs e)
+    {
+      // der Cell Editor
+      TurnCellOnOff( e.X/CELL_SIZE, e.Y/CELL_SIZE);
+      m_panel.Invalidate();
+    }
+
+    // Cells des aktiven Arrays (_CC) zeichnen
+    void DrawCells(Graphics gr)
+    {
+            Brush _br = Brushes.Blue;
+
+            for (int i = 0; i < MAX_CELLS; i++)
+                for (int j = 0; j < MAX_CELLS; j++)
+                {
+                    if (_CC[i, j])
+                        gr.FillRectangle(_br, i * CELL_SIZE , j * CELL_SIZE , (CELL_SIZE ), (CELL_SIZE));
+
+
+                }
+
+        }
+
+        void DrawGrid(Graphics gr)
+    {
+            // Raster zeichnen
+            Pen _pen = Pens.Red;
+
+            // <= damit das Feld einen Abschlussrahmen sein
+            for (int i = 0; i <= MAX_CELLS; i++)
+            {
+                gr.DrawLine(_pen, 0, i * CELL_SIZE, MAX_CELLS * CELL_SIZE, i * CELL_SIZE);//Wagrecht
+
+            }
+
+            for (int i = 0; i <= MAX_CELLS; i++)
+            {
+                gr.DrawLine(_pen, i * CELL_SIZE, 0, i * CELL_SIZE, MAX_CELLS * CELL_SIZE);//senkrecht
+
+            }
+        }
+
+    // Nächste Generation berechnen
+    private void OnStepButton(object sender, EventArgs e)
+    {
+      if (_CC == _CA)
+      {
+        ClearCells(_CB);
+        CalcNextGeneration(_CA, _CB);
+        _CC = _CB;
+      }
+      else
+      {
+        ClearCells(_CA);
+        CalcNextGeneration(_CB, _CA);
+        _CC = _CA;
+      }
+      m_panel.Invalidate();
+    }
+
+    private void OnTimerChk(object sender, EventArgs e)
+    {
+      if (timer1.Enabled)
+        timer1.Enabled = false;
+      else
+        timer1.Enabled = true;
+    }
+
+    private void OnTimer(object sender, EventArgs e)
+    {
+      OnStepButton(null, null);
+    }
+
+    private void OnClearButton(object sender, EventArgs e)
+    {
+      ClearCells(_CA); ClearCells(_CB);
+      m_panel.Invalidate();
+    }
+
+  }
+}
